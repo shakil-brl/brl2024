fd326f7ae65704457754eb74930a3b49bcd0bf0b
maruf-brl <79322030+maruf-229@users.noreply.github.com>
2024-05-05 15:31:25 +0600

    about slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var nextButton = document.querySelector('.carousel-control-next');
 
 var carouselItems = document.querySelectorAll('.carousel-item');
 var numItems = carouselItems.length;
+console.log(numItems);
 var minScreenWidth = 767;
 
 if (window.matchMedia(`(max-width:${minScreenWidth}px)`).matches) {
@@ -35,6 +36,7 @@ if (window.matchMedia(`(max-width:${minScreenWidth}px)`).matches) {
         $(awardCarousel).removeClass('carousel');
     }
 } else {
+    console.log('fff')
     if (numItems > 3) {
         $(awardCarousel).addClass('slide');
     }else{
@@ -42,41 +44,3 @@ if (window.matchMedia(`(max-width:${minScreenWidth}px)`).matches) {
     }
     
 }
-
-
-
-$(document).ready(function() {
-    alert('dsfdsfd')
-    // const sliderTrack = $("#sliderTrack");
-    // const sliderContainer = $(".slider-container");
-
-    // // Calculate the width of the slider container
-    // const containerWidth = sliderContainer.width();
-
-    // // Set the track's initial left position
-    // sliderTrack.css("left", 0);
-
-    // // Function to slide the track
-    // function slide() {
-    //     // Animate the sliding effect
-    //     sliderTrack.animate(
-    //         { left: `-=${containerWidth}` },
-    //         2000, // Duration of each animation in milliseconds (adjust as needed)
-    //         "linear",
-    //         function() {
-    //             // Once the animation completes, move the first image to the end of the track
-    //             const firstImage = $(".slider-item").first();
-    //             sliderTrack.append(firstImage);
-
-    //             // Reset the track's left position to continue the loop
-    //             sliderTrack.css("left", 0);
-
-    //             // Recursively call the function to continue the sliding
-    //             slide();
-    //         }
-    //     );
-    // }
-
-    // // Start the sliding animation
-    // slide();
-});
